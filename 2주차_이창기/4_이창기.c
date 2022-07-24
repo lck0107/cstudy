3f1a48f68543141e9692bd65d6f7781990056583
lck0107 <dlckdrl10720@gmail.com>
2022-07-24 18:35:42 +0900

    수정3

--- 2주차_이창기/4_이창기.c
+++ 2주차_이창기/4_이창기.c
@@ -7,16 +7,18 @@
 
 int main(void) {
 	int result = 1;
-	char arr[1001]={0};
+	char arr[1001] = { 0 };
+	int len;
 	fgets(arr,1001,stdin);
 
-	for (int i = 0; i <= strlen(arr) / 2 -1; i++) {
-		if (arr[i] != arr[strlen(arr) - 1 - i]) {
+	len = strlen(arr);
+
+	for (int i = 0; i <= len / 2 -1; i++) {
+		if (arr[i] != arr[len - 1 - i]) {
 			result = 0;
 			break;
 		}
 	}
-
 	// 위에까지 했는데 YES를 출력하는 법을 몰라서 변수 result를 새로 만들어 출력단계는 따로 추가.
 	if (result == 0)	printf("NO");
 	else if (result == 1)	printf("YES");
